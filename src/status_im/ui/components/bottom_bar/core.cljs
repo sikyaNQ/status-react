b398f6e0c6ad880e839be72b142dd59d4b4aa34b
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2019-02-22 12:00:59 +0200

    new bottom nav design

--- src/status_im/ui/components/bottom_bar/core.cljs
+++ src/status_im/ui/components/bottom_bar/core.cljs
@@ -10,7 +10,8 @@
    [status-im.ui.components.common.common :as components.common]
    [status-im.i18n :as i18n]
    [status-im.ui.components.styles :as common.styles]
-   [re-frame.core :as re-frame]))
+   [re-frame.core :as re-frame]
+   [status-im.ui.components.colors :as colors]))
 
 (defn animate
   ([visible duration to]
@@ -66,14 +67,71 @@
      [react/view
       [content active? count]]]))
 
+(def height 52)
+
+:message
+:dapp
+:wallet
+:profile
+(defn new-tab [{:keys [icon label active?]}]
+  [react/touchable-highlight
+   {:style    {:flex   1
+               :height height}
+    :on-press #(js/alert icon)}
+   [react/view
+    {:style
+     {:flex             1
+      :background-color colors/white
+      :height           height
+      :align-items      :center
+      :justify-content  :space-between
+      :padding-top      6
+      :padding          4}}
+    [vector-icons/icon icon
+     {:color  (if active? colors/blue colors/gray)
+      :height 24
+      :width  24}]
+    [react/view {:style {:align-self      :stretch
+                         :height          14
+                         :align-items     :center
+                         :justify-content :center}}
+     [react/text {:style {:color     (if active? colors/blue colors/gray)
+                          :font-size 11}}
+      label]]]])
+
 (defn tabs [current-view-id]
-  [react/view {:style tabs.styles/tabs-container}
-   (for [{:keys [content view-id accessibility-label count-subscription]} tabs-list]
-     ^{:key view-id} [tab view-id content (= view-id current-view-id) accessibility-label count-subscription])])
+  [react/view
+   {:style {:height         height
+            :align-self     :stretch
+            :shadow-radius  4
+            :shadow-offset  {:width 0 :height -5}
+            :shadow-opacity 0.3
+            :shadow-color   "rgba(0, 9, 26, 0.12)"}}
+   [react/view {:style {:height         height
+                        :align-self     :stretch
+                        :background-color colors/white
+                        :padding-left    8
+                        :padding-right   8
+                        :flex-direction :row}}
+    [new-tab {:icon    :main-icons/message
+              :label   "Chats"
+              :active? (= current-view-id :chat-stack)}]
+    [new-tab {:icon    :main-icons/dapp
+              :label   "DApps"
+              :active? (= current-view-id :dapp-stack)}]
+    [new-tab {:icon    :main-icons/wallet
+              :label   "Wallet"
+              :active? (= current-view-id :wallet-stack)}]
+    [new-tab {:icon    :main-icons/profile
+              :label   "Profile"
+              :active? (= current-view-id :profile-stack)}]]]
+  #_[react/view {:style tabs.styles/tabs-container}
+     (for [{:keys [content view-id accessibility-label count-subscription]} tabs-list]
+       ^{:key view-id} [tab view-id content (= view-id current-view-id) accessibility-label count-subscription])])
 
 (defn tabs-animation-wrapper [visible? keyboard-shown? tab]
   [react/animated-view
-   {:style {:height    tabs.styles/tabs-height
+   {:style {:height    height
             :bottom    0
             :left      0
             :right     0
@@ -82,8 +140,7 @@
                          (animation/interpolate
                           visible?
                           {:inputRange  [0 1]
-                           :outputRange [tabs.styles/tabs-height
-                                         0]})}]}}
+                           :outputRange [height 0]})}]}}
    [react/safe-area-view [tabs tab]]])
 
 (def disappearance-duration 150)
